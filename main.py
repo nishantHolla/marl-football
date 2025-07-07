73a4300555be8011c6958ac25dd5dc9ce374729d
Nishant Holla <nishantholla19@gmail.com>
2025-07-07 22:19:24 +0530

    Update main

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from train.dqn_train_v1 import train
 from evaluate.dqn_evaluate_v1 import evaluate
 
 if len(sys.argv) != 2:
-    print("Usage: python main.py [run|train|eval]")
+    print("Usage: python main.py [ debug | train |eval ]")
     sys.exit(1)
 
 if sys.argv[1] == "debug":
     debug()
 
 elif sys.argv[1] == "train":
-    train("test", 1000)
+    train("test", 10000)
 
 elif sys.argv[1] == "eval":
-    evaluate("test_1000", 10, debug=False)
+    evaluate("test_3000", 10, debug=False)
